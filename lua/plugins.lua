1210387b040b4cd799ac725b374cc92abcd63169
jrupinski <rupinskijakub@gmail.com>
2026-02-22 17:47:02 +0100

    Oil: re-adjust settings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -64,31 +64,31 @@ vim.pack.add({
 })
 require("mini.icons").setup()
 require("oil").setup({
-  columns = {
-    "icon",
-    -- "permissions",
-    -- "size",
-    -- "mtime",
-  },
-  keymaps = {
-    -- -- Do not override tmux keymaps
-    -- ['<C-h>'] = false,
-    -- ['<C-l>'] = false,
-    -- ['<C-k>'] = false,
-    -- ['<C-j>'] = false,
-    ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-    ['<C-s>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
-    ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
-    ['yp'] = {
-      callback = function()
-        require('oil.actions').copy_entry_path.callback()
-        vim.fn.setreg('+', vim.fn.getreg(vim.v.register))
-      end,
-      desc = 'Copy filepath to system clipboard',
+    default_file_explorer = true,
+    columns = {
+      "icon"
     },
-  },
-  view_options = {
-    show_hidden = true,
-  },
-}
-)
+    constrain_cursor = "name",
+    watch_for_changes = true,
+    keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    view_options = {
+        show_hidden = true,
+    },
+})
