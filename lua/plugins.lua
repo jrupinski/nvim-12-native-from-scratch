b5566c8955b176afa036efc01abca9ea041247b2
jrupinski <rupinskijakub@gmail.com>
2026-02-23 11:31:35 +0100

    Remove custom keymaps from blink, use defaults

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -70,19 +70,6 @@ require('blink.cmp').setup({
   signature = { enabled = true },
   keymap = {
     preset = "default",
-    ["<C-space>"] = {},
-    ["<C-p>"] = {},
-    ["<Tab>"] = {},
-    ["<S-Tab>"] = {},
-    ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
-    ["<C-n>"] = { "select_and_accept" },
-    ["<C-k>"] = { "select_prev", "fallback" },
-    ["<C-j>"] = { "select_next", "fallback" },
-    ["<C-b>"] = { "scroll_documentation_down", "fallback" },
-    ["<C-f>"] = { "scroll_documentation_up", "fallback" },
-    ["<C-l>"] = { "snippet_forward", "fallback" },
-    ["<C-h>"] = { "snippet_backward", "fallback" },
-    -- ["<C-e>"] = { "hide" },
   },
 
   appearance = {
@@ -106,7 +93,6 @@ require('blink.cmp').setup({
 
   sources = { default = { "lsp" } }
 })
-
 
 -- File navigation
 vim.pack.add({
