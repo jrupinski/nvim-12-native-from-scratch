037a166a781b6280e6d2806e3a666e2b628ca0eb
jrupinski <rupinskijakub@gmail.com>
2026-02-24 17:24:55 +0100

    Add plugin: fuzzy-finding (fzf-lua, instead of telescope)

---
+++ lua/plugins/fuzzy-finder.lua
@@ -0,0 +1,25 @@
+vim.pack.add({
+  { src = "https://github.com/ibhagwan/fzf-lua" },
+  { src = "https://github.com/nvim-mini/mini.icons" },
+}
+)
+-- Copy over kickstart telescope keymaps, just switch provider to fzf-lua and adjust some calls.
+local builtin = require 'fzf-lua'
+-- Register fzf-lua as the UI interface for vim.ui.select
+builtin.register_ui_select()
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sf', builtin.files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sq', builtin.quickfix, { desc = '[S]earch [Q]uickfix list' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect fzf-lua built-in commands' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_cword, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics_document, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+vim.keymap.set('n', '<leader>/', builtin.lgrep_curbuf, { desc = '[/] Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>sn', function()
+  builtin.files({ cwd = vim.fn.stdpath 'config' })
+end, { desc = '[S]earch [N]eovim config files' })
