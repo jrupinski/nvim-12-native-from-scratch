d0018b8d8b81a43a121184d166641f9f0d99bacc
jrupinski <rupinskijakub@gmail.com>
2026-02-24 19:51:31 +0100

    Adjust vim-test keymaps

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -13,8 +13,8 @@ vim.g['test#ruby#rails#executable'] = 'dip test'
 -- vim.g['test#ruby#executable'] = 'dip test'
 
 local map = vim.keymap.set
-map('n', '<leader>t', ':TestNearest<CR>', { desc = 'Test: Nearest' })
-map('n', '<leader>T', ':TestFile<CR>', { desc = 'Test: File' })
-map('n', '<leader>a', ':TestSuite<CR>', { desc = 'Test: Suite' })
-map('n', '<leader>l', ':TestLast<CR>', { desc = 'Test: Last' })
-map('n', '<leader>g', ':TestVisit<CR>', { desc = 'Test: Visit' })
+map('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest' })
+map('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' })
+map('n', '<leader>ta', ':TestSuite<CR>', { desc = '[T]est Suite [A]ll' })
+map('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' })
+map('n', '<leader>tv', ':TestVisit<CR>', { desc = '[T]est [V]isit' })
