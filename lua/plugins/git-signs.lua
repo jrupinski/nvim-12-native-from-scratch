8ea8993b44e24bff9d8efd732df2abfda840c8d3
jrupinski <rupinskijakub@gmail.com>
2026-02-24 19:52:38 +0100

    Remove toggle gitsigns keymaps

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -3,6 +3,7 @@ vim.pack.add({
 })
 
 require('gitsigns').setup({
+  current_line_blame = true,
   on_attach = function(bufnr)
     local gitsigns = require 'gitsigns'
 
@@ -49,8 +50,6 @@ require('gitsigns').setup({
     map('n', '<leader>hD', function()
       gitsigns.diffthis '@'
     end, { desc = 'git [D]iff against last commit' })
-    -- Toggles
-    map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-    map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+
   end,
 })
