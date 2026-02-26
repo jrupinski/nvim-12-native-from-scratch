4fdddaf2ed20e9edf19135bf15c446f343be8bfa
jrupinski <rupinskijakub@gmail.com>
2026-02-26 11:59:34 +0100

    Add plugin: Syntax highlighting via treesitter (+ matchup to see current block context)

---
+++ lua/plugins/syntax-highlighting.lua
@@ -0,0 +1,8 @@
+vim.pack.add({
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-context' },
+  { src = 'https://github.com/andymass/vim-matchup' },
+})
+require('nvim-treesitter').install({ 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'ruby', 'query', 'vim', 'vimdoc' })
+vim.g.matchup_matchparen_offscreen = { method = "popup" }
