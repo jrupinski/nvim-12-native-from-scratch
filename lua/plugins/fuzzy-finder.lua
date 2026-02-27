2b1e4473d755bee0a4be9c81fda53b0d0fb3527f
jrupinski <rupinskijakub@gmail.com>
2026-02-27 12:56:12 +0100

    LSP: Use fzf-lua to represent LSP results nicely

--- lua/plugins/fuzzy-finder.lua
+++ lua/plugins/fuzzy-finder.lua
@@ -23,3 +23,43 @@ vim.keymap.set('n', '<leader>/', builtin.lgrep_curbuf, { desc = '[/] Fuzzily sea
 vim.keymap.set('n', '<leader>sn', function()
   builtin.files({ cwd = vim.fn.stdpath 'config' })
 end, { desc = '[S]earch [N]eovim config files' })
+
+
+-- LSP keymaps
+local lsp = vim.lsp.buf
+-- Rename the variable under your cursor.
+--  Most Language Servers support renaming across files, etc.
+vim.keymap.set('n', 'grn', lsp.rename, { silent = true, desc = 'LSP: [R]e[n]ame' })
+
+-- Execute a code action, usually your cursor needs to be on top of an error
+-- or a suggestion from your LSP for this to activate.
+vim.keymap.set({ 'n', 'x' }, 'gra', lsp.code_action, { silent = true, desc = '[G]oto Code [A]ction' })
+
+-- Find references for the word under your cursor.
+vim.keymap.set('n', 'grr', builtin.lsp_references, { silent = true, desc = 'LSP: [G]oto [R]eferences' })
+
+-- Jump to the implementation of the word under your cursor.
+--  Useful when your language has ways of declaring types without an actual implementation.
+vim.keymap.set('n', 'gri', builtin.lsp_implementations, { silent = true, desc = 'LSP: [G]oto [I]mplementation' })
+
+-- Jump to the definition of the word under your cursor.
+--  This is where a variable was first declared, or where a function is defined, etc.
+--  To jump back, press <C-t>.
+vim.keymap.set('n', 'grd', builtin.lsp_definitions, { silent = true, desc = 'LSP: [G]oto [D]efinition' })
+
+-- WARN: This is not Goto Definition, this is Goto Declaration.
+--  For example, in C this would take you to the header.
+vim.keymap.set('n', 'grD', builtin.lsp_declarations, { silent = true, desc = 'LSP: [G]oto [D]eclaration' })
+
+-- Fuzzy find all the symbols in your current document.
+--  Symbols are things like variables, functions, types, etc.
+vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { silent = true, desc = 'LSP: Open Document Symbols' })
+
+-- Fuzzy find all the symbols in your current workspace.
+--  Similar to document symbols, except searches over your entire project.
+vim.keymap.set('n', 'gW', builtin.lsp_workspace_symbols, { silent = true, desc = 'LSP: Open Workspace Symbols' })
+
+-- Jump to the type of the word under your cursor.
+--  Useful when you're not sure what type a variable is and you want to see
+--  the definition of its *type*, not where it was *defined*.
+vim.keymap.set('n', 'grt', builtin.lsp_typedefs, { silent = true, desc = 'LSP: [G]oto [T]ype Definition' })
