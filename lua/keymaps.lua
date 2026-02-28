1c471bfb31db42c0ece73c9e06cb2d62da987136
jrupinski <rupinskijakub@gmail.com>
2026-02-28 01:14:28 +0100

    Keymaps: Add missing descriptions

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,27 +9,26 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Disable space in normal mode, just in case of legacy behavior
-keymap("n", "<space>", "<Nop>")
+keymap("n", "<space>", "<Nop>", { desc = 'Disable space default' })
 
 -- Update packages
-keymap("n", "<leader>ps", '<cmd>lua vim.pack.update()<CR>')
+keymap("n", "<leader>ps", '<cmd>lua vim.pack.update()<CR>', { desc = '[p]ackage [s]ync/update' })
 
 -- Some keymaps from a minimalistic setup - TBD if they'll be useful QOL change
 keymap("n", "j", function()
   return tonumber(vim.api.nvim_get_vvar("count")) > 0 and "j" or "gj"
-end, { expr = true, silent = true }) -- Move down, but use 'gj' if no count is given
+end, { expr = true, silent = true, desc = 'smart down (gj)' })
 keymap("n", "k", function()
   return tonumber(vim.api.nvim_get_vvar("count")) > 0 and "k" or "gk"
-end, { expr = true, silent = true })                         -- Move up, but use 'gk' if no count is given
-keymap("n", "<C-d>", "<C-d>zz")                              -- Scroll down and center the cursor
-keymap("n", "<C-u>", "<C-u>zz")                              -- Scroll up and center the cursor
-keymap("n", "<Leader>w", "<cmd>w!<CR>", s)                   -- Save the current file
-keymap("n", "<Leader>te", "<cmd>tabnew<CR>", s)              -- Open a new tab
-keymap("n", "<Leader>%", "<cmd>vsplit<CR>", s)               -- Split the window vertically
-keymap("n", "<Leader>\"", "<cmd>split<CR>", s)               -- Split the window horizontally
-keymap("n", "<Leader>f", ":lua vim.lsp.buf.format()<CR>", s) -- Format the current buffer using LSP
-keymap("v", "p", '"_dP')                                     -- Paste without overwriting the default register
-keymap("t", "<Esc>", "<C-\\><C-N>")                          -- Exit terminal mode
--- Change directory to the current file's directory
-keymap("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>')
+end, { expr = true, silent = true, desc = 'smart up (gk)' })
+keymap("n", "<C-d>", "<C-d>zz", { desc = 'scroll [d]own centered' })
+keymap("n", "<C-u>", "<C-u>zz", { desc = 'scroll [u]p centered' })
+keymap("n", "<Leader>w", "<cmd>w!<CR>", { silent = true, desc = '[w]rite file (force)' })
+keymap("n", "<Leader>te", "<cmd>tabnew<CR>", { silent = true, desc = '[t]ab n[e]w' })
+keymap("n", "<Leader>%", "<cmd>vsplit<CR>", { silent = true, desc = 'split [%%] vertical' })
+keymap("n", "<Leader>\"", "<cmd>split<CR>", { silent = true, desc = 'split ["] horizontal' })
+keymap("n", "<Leader>f", ":lua vim.lsp.buf.format()<CR>", { silent = true, desc = '[f]ormat buffer (LSP)' })
+keymap("v", "p", '"_dP', { desc = '[p]aste without yank' })
+keymap("t", "<Esc>", "<C-\\><C-N>", { desc = 'exit terminal mode' })
+keymap("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>', { desc = '[c]hange [d]irectory to file' })
 
