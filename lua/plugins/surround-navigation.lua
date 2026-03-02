14ca3b7630de99c10ca99958ca6bf67344a9dfe4
jrupinski <rupinskijakub@gmail.com>
2026-03-02 14:05:23 +0100

    Add plugin: Use mini-surround instead of nvim-surround

--- lua/plugins/surround-navigation.lua
+++ lua/plugins/surround-navigation.lua
@@ -1,5 +1,5 @@
 
 vim.pack.add({
-  { src = 'https://github.com/kylechui/nvim-surround' },
+  { src = 'https://github.com/nvim-mini/mini.surround' },
 })
-require("nvim-surround").setup()
+require('mini.surround').setup()
