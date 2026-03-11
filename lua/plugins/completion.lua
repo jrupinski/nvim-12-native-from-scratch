ce5e5bd51b8d24f16cf23a5a0a70d6c7d959f30a
jrupinski <rupinskijakub@gmail.com>
2026-03-11 12:26:16 +0100

    Completion - add snippets, remove settings that are already default

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,29 +1,27 @@
 vim.pack.add({
   { src = "https://github.com/saghen/blink.cmp", version = 'v1.9.1' },
+  { src = "https://github.com/rafamadriz/friendly-snippets" }
 })
 require('blink.cmp').setup({
   fuzzy = { implementation = 'prefer_rust_with_warning' },
   signature = { enabled = true },
-  keymap = {
-    preset = "default",
-  },
 
-  appearance = {
-    use_nvim_cmp_as_default = true,
-    nerd_font_variant = "mono",
-  },
-
+  -- Auto show documentation for completions
   completion = {
     documentation = {
       auto_show = true,
-      auto_show_delay_ms = 200,
+      auto_show_delay_ms = 0
     }
   },
 
   cmdline = {
+    -- Auto show completions for command mode
+    completion = { menu = { auto_show = true } },
     keymap = {
       preset = 'inherit',
-      ['<CR>'] = { 'accept_and_enter', 'fallback' },
+      -- ['<CR>'] = { 'accept_and_enter', 'fallback' },
+      -- recommended, as the default keymap will only show and select the next item
+      ['<Tab>'] = { 'show', 'accept' },
     },
   },
 
@@ -35,6 +33,20 @@ require('blink.cmp').setup({
         module = "lazydev.integrations.blink",
         score_offset = 100,
       },
+
+      snippets = {
+        opts = {
+          -- see the list of frameworks in: https://github.com/rafamadriz/friendly-snippets/tree/main/snippets/frameworks
+          -- and search for possible languages in: https://github.com/rafamadriz/friendly-snippets/blob/main/package.json
+          -- the following is just an example, you should only enable the frameworks that you use
+          extended_filetypes = {
+            markdown = { 'jekyll' },
+            sh = { 'shelldoc' },
+            ruby = { "rails" },
+            eruby = { "html", "rails" },
+          }
+        }
+      }
     },
   },
 })
